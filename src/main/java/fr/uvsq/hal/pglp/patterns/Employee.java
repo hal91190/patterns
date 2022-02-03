2e6c46a4a9ef984204b97b6b20e206092f94f87f
Stéphane Lopes <stephane.lopes@uvsq.fr>
2022-02-03 11:52:10 +0100

    Initialise le builder pour un personnel

--- src/main/java/fr/uvsq/hal/pglp/patterns/Employee.java
+++ src/main/java/fr/uvsq/hal/pglp/patterns/Employee.java
@@ -26,6 +26,12 @@ public class Employee {
     this.birthDate = birthDate;
   }
 
+  private Employee(Builder builder) {
+    firstname = builder.firstname;
+    lastname = builder.lastname;
+    birthDate = builder.birthDate;
+  }
+
   public String getFirstname() {
     return firstname;
   }
@@ -36,5 +42,31 @@ public class Employee {
 
   public LocalDate getBirthDate() {
     return birthDate;
+  }
+
+  /**
+   * Permet l'initialisation d'un personnel selon le pattern Builder.
+   */
+  public static class Builder {
+    private final String firstname;
+    private final String lastname;
+    private final LocalDate birthDate;
+
+    /**
+     * Fournit les attributs obligatoires pour un personnel.
+     *
+     * @param firstname prénom
+     * @param lastname nom
+     * @param birthDate date de naissance
+     */
+    public Builder(String firstname, String lastname, LocalDate birthDate) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.birthDate = birthDate;
+    }
+
+    public Employee build() {
+      return new Employee(this);
+    }
   }
 }
--- src/test/java/fr/uvsq/hal/pglp/patterns/EmployeeTest.java
+++ src/test/java/fr/uvsq/hal/pglp/patterns/EmployeeTest.java
@@ -14,4 +14,12 @@ class EmployeeTest {
     assertEquals("Sacquet", employee.getLastname());
     assertEquals(LocalDate.of(1987, 6, 12), employee.getBirthDate());
   }
+
+  @Test
+  public void unPersonnelPossedeLesCaracteristiquesObligatoiresAvecBuilder() {
+    Employee employee = new Employee.Builder("Frodon", "Sacquet", LocalDate.of(1987, 6, 12)).build();
+    assertEquals("Frodon", employee.getFirstname());
+    assertEquals("Sacquet", employee.getLastname());
+    assertEquals(LocalDate.of(1987, 6, 12), employee.getBirthDate());
+  }
 }
